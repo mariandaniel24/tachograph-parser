41817393f8f21e7a795e808a6143b61bb92fd69d
Daniel Stelea <mariandaniel2497@gmail.com>
2024-10-23 14:15:22 +0300

    (ts_bindings): napi 2

--- crates/ts_bindings/__test__/perf.spec.ts
+++ crates/ts_bindings/__test__/perf.spec.ts
@@ -1,13 +1,11 @@
 import { it } from "node:test";
-import assert from "node:assert";
+import { strict as assert } from "node:assert";
 import { parseVu, parseCard, detectTachoFileType } from "../index.js";
 import { Buffer } from "node:buffer";
-import fs from "node:fs";
+import { readFileSync } from "node:fs";
 
 it("should parse a tacho file with parseCard in under 100ms", async () => {
-	const tachoFileBuffer = Buffer.from(
-		fs.readFileSync("../../data/card_gen2.ddd"),
-	);
+	const tachoFileBuffer = Buffer.from(readFileSync("../../data/card_gen2.ddd"));
 	const start = performance.now();
 	const parsed = parseCard(tachoFileBuffer);
 	const end = performance.now();
@@ -19,9 +17,7 @@ it("should parse a tacho file with parseCard in under 100ms", async () => {
 	assert(duration < 100_000_000);
 });
 it("should parse a tacho file with parseVu in under 100ms", async () => {
-	const tachoFileBuffer = Buffer.from(
-		fs.readFileSync("../../data/vu_gen2.ddd"),
-	);
+	const tachoFileBuffer = Buffer.from(readFileSync("../../data/vu_gen2.ddd"));
 	const start = performance.now();
 	const parsed = parseVu(tachoFileBuffer);
 	const end = performance.now();
@@ -31,9 +27,7 @@ it("should parse a tacho file with parseVu in under 100ms", async () => {
 	assert(duration < 100_000_000);
 });
 it("should detect a tacho file type in under 100ms", async () => {
-	const tachoFileBuffer = Buffer.from(
-		fs.readFileSync("../../data/card_gen2.ddd"),
-	);
+	const tachoFileBuffer = Buffer.from(readFileSync("../../data/card_gen2.ddd"));
 	const start = performance.now();
 	const parsed = detectTachoFileType(tachoFileBuffer);
 	const end = performance.now();
